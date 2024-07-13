40d987f371fd5f5464fc2678662fc57c0691dd88
HifaOner <hifaoner@gmail.com>
2024-07-13 19:05:37 +0300

    onboarding page

--- lib/views/onboarding.dart
+++ lib/views/onboarding.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:oua_flutter_travel_gallery/components/onboarding_data.dart';
+import 'package:oua_flutter_travel_gallery/views/bottom_nav.dart';
+import 'package:path/path.dart' as Path;
 
 class OnboardingPage extends StatefulWidget {
   const OnboardingPage({super.key});
@@ -11,13 +13,18 @@ class OnboardingPage extends StatefulWidget {
 }
 
 class _OnboardingPageState extends State<OnboardingPage> {
+
   final controller = OnboardingData();
   final pageController = PageController();
   int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [body()]),
+      body: Column(children: [
+        body(),
+        buildDots(),
+        button(),
+      ]),
     );
   }
 
@@ -53,12 +60,13 @@ class _OnboardingPageState extends State<OnboardingPage> {
                         textAlign: TextAlign.center,
                       ),
                       Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 25),
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
                         child: Text(
                           controller.items[currentIndex].description,
                           style: const TextStyle(
-                              color: Color.fromARGB(255, 141, 139, 139),
-                              fontFamily: "Poppins"),
+                              color: Color.fromARGB(255, 182, 178, 178),
+                              fontFamily: "Poppins",
+                              fontSize: 17),
                           textAlign: TextAlign.center,
                         ),
                       ),
@@ -70,4 +78,65 @@ class _OnboardingPageState extends State<OnboardingPage> {
       ),
     );
   }
+
+  // Dots
+  Widget buildDots() {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: List.generate(
+            controller.items.length,
+            (index) => AnimatedContainer(
+                margin: const EdgeInsets.symmetric(horizontal: 2),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  color: currentIndex == index ? Colors.blue : Colors.grey,
+                ),
+                height: 7,
+                width: currentIndex == index ? 30 : 7,
+                duration: const Duration(milliseconds: 700))),
+      ),
+    );
+  }
+
+  // Button
+  Widget button() {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 20),
+      width: MediaQuery.of(this.context).size.width * .9,
+      height: 55,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        color: const Color.fromARGB(255, 48, 145, 201),
+      ),
+      
+      child: TextButton(
+        onPressed: (){
+          setState(() {
+            if(
+            currentIndex != controller.items.length -1) {currentIndex++;}
+            else {
+              navigateToNextScreen();
+            };
+          });
+        },
+        child: Text(currentIndex == controller.items.length - 1
+            ? "Get Started"
+            : "Continue",
+            style: const TextStyle(color: Colors.white),),
+      ),
+    );
+  }
+
+
+
+  void navigateToNextScreen() {
+  Navigator.push(
+    this.context,
+    MaterialPageRoute(
+      builder: (context) => const BottomNav(),
+    ),
+  );
+}
 }
